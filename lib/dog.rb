a83520dd7bd470b3a1cda9ba3b044b792be71db3
Shea Kusiak <shkusiak@gmail.com>
2017-10-21 03:35:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 # Your code goes here!
 class Dog
-  def name
-    @name
+  def initialize(name, bark)
+    @name = name
+    @bark = bark
   end
 end
 
-name.instance_variable_set(:@name, "Fido")
+#name.instance_variable_set(:@name, "Fido")
